daaa0d709922e5a947845a2808662da60a7c5e5a
Qlbbl1111 <69528541+Qlbbl1111@users.noreply.github.com>
2022-01-27 03:31:58 +0000

    Added Dance and Yell commands.

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import os
+import discord
 from keep_alive import keep_alive
 from discord.ext import commands
 import random
 
 
 token = os.environ['TOKEN']
+author_id = os.environ['author_id']
+dancers = ['https://media.giphy.com/media/tsX3YMWYzDPjAARfeg/giphy.gif', 'https://media.giphy.com/media/14qb1Uhf40ndw4/giphy.gif', 'https://media.giphy.com/media/l2QZSjo0lwEMC0GVG/giphy.gif', 'https://media.giphy.com/media/5xaOcLGvzHxDKjufnLW/giphy.gif']
+
 
 bot = commands.Bot(
 	command_prefix="~",  # Change to desired prefix
 	case_insensitive=True  # Commands aren't case-sensitive
 )
 
-bot.author_id = 318122785807269888  # Change to your discord id!!!
+bot.author_id = author_id  # Change to your discord id!!!
+
 
 @bot.event 
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
 
-@bot.command()
-async def test2(ctx, arg1, arg2):
-    await ctx.send('You passed {} and {}'.format(arg1, arg2))
+#Random dance command.
+@bot.command(name='dance', help='Sends random dance gif.')
+async def dance(ctx):
+    await ctx.send(random.choice(dancers))
 
+@dance.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Error: Something went wrong.')
+
+#Yell command.
+@bot.command(name='yell', help='Yell at another member.')
+async def yell(ctx, *, member:discord.Member):
+  await ctx.send(f"Dear <@{member.id}>, AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!")
+
+@yell.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Error: There is no member by that name.')
+
+
+#other command
 @bot.command()
 async def add(ctx, a: int, b: int):
     await ctx.send(a + b)
-
 
 
 extensions = [
