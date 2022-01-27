adc2cb5759e37e907f0ee285db67a618fd98472b
Qlbbl1111 <69528541+Qlbbl1111@users.noreply.github.com>
2022-01-27 05:31:16 +0000

    Final Commit 01/26/22. Insult, Yell, Dance Commands and Keep Alive.

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import random
 
 token = os.environ['TOKEN']
 author_id = os.environ['author_id']
+
 dancers = ['https://media.giphy.com/media/tsX3YMWYzDPjAARfeg/giphy.gif', 'https://media.giphy.com/media/14qb1Uhf40ndw4/giphy.gif', 'https://media.giphy.com/media/l2QZSjo0lwEMC0GVG/giphy.gif', 'https://media.giphy.com/media/5xaOcLGvzHxDKjufnLW/giphy.gif']
 
+insults = [' smells like rotten fish.', ' is kind of like Jim Carrey\'s grinch; but uglier and more of a dick.', ', fuck you.', ' \"little ho, little bitch, suck my 5.3\" dick\"', ', you know what i did last night? Ya that\'s right, I fucked your mom.', ' Go back to where you came from. You should go climb right back into your dad\'s hairy ballsack you ugly mofo.', ' Calling all men/women: If you think a blob fish is hot, then you\'ll love this guy. \nhttps://media.giphy.com/media/QGBWk7DnckEN2/giphy.gif ']
 
+#Command prefix.
 bot = commands.Bot(
 	command_prefix="~",  # Change to desired prefix
 	case_insensitive=True  # Commands aren't case-sensitive
@@ -27,29 +30,33 @@ async def on_ready():  # When the bot is ready
 #Random dance command.
 @bot.command(name='dance', help='Sends random dance gif.')
 async def dance(ctx):
-    await ctx.send(random.choice(dancers))
+    await ctx.send(random.sample(dancers, 1)[0])
 
 @dance.error
-async def info_error(ctx, error):
+async def dance_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send('Error: Something went wrong.')
 
 #Yell command.
-@bot.command(name='yell', help='Yell at another member.')
+@bot.command(name='yell', help='Yell at someone.')
 async def yell(ctx, *, member:discord.Member):
   await ctx.send(f"Dear <@{member.id}>, AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!")
 
 @yell.error
-async def info_error(ctx, error):
+async def yell_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send('Error: There is no member by that name.')
 
 
-#other command
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a + b)
+#Insult command
+@bot.command(name='insult', help='Insult another member.')
+async def insult(ctx, *, member:discord.Member):
+  await ctx.send(f"<@{member.id}>{random.sample(insults, 1)[0]}")
 
+@insult.error
+async def insult_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Error: There is no member by that name.')
 
 extensions = [
 	'cogs.cog_example'  # Same name as it would be if you were importing it
@@ -59,7 +66,7 @@ if __name__ == '__main__':  # Ensures this is the file being ran
 	for extension in extensions:
 		bot.load_extension(extension)  # Loades every extension.
 
-#keep_alive()  # Starts a webserver to be pinged.
+keep_alive()  # Starts a webserver to be pinged.
 
 
 bot.run(token)  # Starts the bot
